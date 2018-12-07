234fe0ae49b8893204d98ed98f479a5419b416c8
ryym <ryym.64@gmail.com>
2018-12-07 16:22:30 +0900

    Start implementing RatingControl

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,31 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by ryu on 2018/12/07.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    // MARK: Initialization
+
+    // Called when programmatically initialized.
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    // Called when loaded from the storyboard.
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
