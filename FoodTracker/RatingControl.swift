b69fa9235c0d207338ba22a017baee706b2ceed8
ryym <ryym.64@gmail.com>
2018-12-10 07:57:36 +0900

    Show multiple red buttons in rating control
    幅や高さの設定がチュートリアル通りにいかない。
    幅は自動で Stack View いっぱいになるし、ボタンの数を増やしても
    均等に幅が狭まったりしない。
    
    よくわからないけど次にいく。
    
    https://developer.apple.com/library/archive/referencelibrary/GettingStarted/DevelopiOSAppsSwift/ImplementingACustomControl.html#//apple_ref/doc/uid/TP40015214-CH19-SW1

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -7,17 +7,68 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+// IBDesignable lets Interface Builder draw this control.
+@IBDesignable class RatingControl: UIStackView {
+    // MARK: Properties
+
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
+
+    private var ratingButtons = [UIButton]()
+
+    var rating = 0
+
     // MARK: Initialization
 
     // Called when programmatically initialized.
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
 
     // Called when loaded from the storyboard.
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
+    }
+
+    private func setupButtons() {
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+
+        for _ in 0..<starCount {
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+
+            // Add constraints.
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+
+            addArrangedSubview(button)
+
+            ratingButtons.append(button)
+        }
+    }
+
+    // MARK: Button Action
+
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Button pressed :+1:")
     }
 
     /*
