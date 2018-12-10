792c136c2eaaab0ef55689722c153212fe3467c2
ryym <ryym.64@gmail.com>
2018-12-10 16:44:19 +0900

    Remove unnecessary 'Set Default Text' button and tweak view

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,10 +22,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
 
     // MARK: Actions
-
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
 
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
         nameTextField.resignFirstResponder()
